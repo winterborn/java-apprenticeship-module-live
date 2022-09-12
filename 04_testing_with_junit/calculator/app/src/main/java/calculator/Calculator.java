f16cb121bef2b9459356871f8e6f68119a6dfb50
Philip White <phwhite89@gmail.com>
2022-09-12 13:36:08 +0100

    completed calculator test and implemented method.

---
+++ 04_testing_with_junit/calculator/app/src/main/java/calculator/Calculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+class Calculator {
+    public static void main(String[] args) {
+      
+    }
+
+    Integer add(int integer1, int integer2) {
+      return integer1 + integer2;
+    }
+  }
