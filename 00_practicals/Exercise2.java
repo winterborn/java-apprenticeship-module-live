e59c4f0e97c28ddcda0d17cbe3a0d01687224df0
Philip White <phwhite89@gmail.com>
2022-09-12 17:44:52 +0100

    completed exercise 2.

--- 00_practicals/Exercise2.java
+++ 00_practicals/Exercise2.java
@@ -1,19 +1,55 @@
 // Example 2:
 
+// import java.util.ArrayList;
+
+// public class App {
+//   public static void main(String[] args) {
+//     ArrayList<> names = new ArrayList<>();
+
+//     names.add("Kyle");
+//     names.add("Sophie");
+//     names.add("Graeme");
+
+//     nameToFind = "Lisa";
+
+//     App app = new App();
+//     result = app.isInArray(names, nameToFind);
+
+//     if (result) {
+//       System.out.println("The name is in the array. Yay!");
+//     } else {
+//       System.out.println("The name couldn't be found in the array — sorry...");
+//     }
+//   }
+
+//   public isInArray(names, nameToFind) {
+//     for (String name : names) {
+//       if (name == nameToFind) {
+//         return true;
+//       }
+//     }
+
+//     return false;
+//   } 
+// }
+
+
+// Example 2: Fixed missing String nameToFind, Boolean result and boolean isInArray(ArrayList<String>, String nameToFind)
+
 import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
-    ArrayList<> names = new ArrayList<>();
+    ArrayList<String> names = new ArrayList<String>();
 
     names.add("Kyle");
     names.add("Sophie");
     names.add("Graeme");
 
-    nameToFind = "Lisa";
+    String nameToFind = "Lisa";
 
     App app = new App();
-    result = app.isInArray(names, nameToFind);
+    Boolean result = app.isInArray(names, nameToFind);
 
     if (result) {
       System.out.println("The name is in the array. Yay!");
@@ -22,7 +58,7 @@ public class App {
     }
   }
 
-  public isInArray(names, nameToFind) {
+  public boolean isInArray(ArrayList<String> names, String nameToFind) {
     for (String name : names) {
       if (name == nameToFind) {
         return true;
